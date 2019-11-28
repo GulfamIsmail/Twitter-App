671cd4319cfe6aa43c917c789a259c0e380bc740
Gulfam Ismail <gulfamismail09@gmail.com>
2019-11-28 16:48:41 +0500

    log-in-log-out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ end
 def create
 @user = User.new(user_params)
 if @user.save
+	log_in @user
 	flash[:success] = "Welcome to the Sample App!"
 	redirect_to @user
 # Handle a successful save.
@@ -21,7 +22,4 @@ def user_params
 params.require(:user).permit(:name, :email, :password,
 :password_confirmation)
 end
-
-
-
 end

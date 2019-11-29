9170fb332fab3f47818191fb28bc4e81ea00c77b
Gulfam Ismail <gulfamismail09@gmail.com>
2019-11-29 18:14:09 +0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ def create
 user = User.find_by(email: params[:session][:email].downcase)
 if user && user.authenticate(params[:session][:password])
 	log_in user
-	remember user
-redirect_to user
+	params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+redirect_back_or user
 # Log the user in and redirect to the user's show page.
 else
 # Create an error message.
@@ -15,7 +15,7 @@ render 'new'
 end
 end
 def destroy
-	log_out
+	    log_out
 redirect_to root_url
 end
 end

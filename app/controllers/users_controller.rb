713b4ec9b3ff5ea0527c577164ed19c6de630242
Gulfam Ismail <gulfamismail09@gmail.com>
2019-12-05 19:19:57 +0500

    def show changing and 100% correct

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,13 +20,11 @@ end
    @user = User.find(params[:id])
 end
 def create
-@user = User.new(user_params)
-flash[:info] = @user.errors.messages
-
- if @user.save
-	
-flash[:info] = "User has been created successfully"
-redirect_to root_url	
+  @user = User.new(user_params)
+  if @user.save
+    UserMailer.account_activation(@user).deliver_now
+    flash[:info] = "Please check your email to activate your account."
+    redirect_to root_url
 else
 render 'new'
 end
